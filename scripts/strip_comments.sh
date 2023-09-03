9551f6240c3952090987aa425fe0b4a421cc2e8c
Bryce Benton <brycebenton+pantheon2@gmail.com>
2023-09-03 12:29:42 -0500

    no comments1

--- scripts/strip_comments.sh
+++ scripts/strip_comments.sh
@@ -1,2 +1,2 @@
-find /Users/bryce/releaf.static/releaf.site -maxdepth 2 -name "*.html" | xargs -I {} sed -i '' '/<!--/,/-->/d' {}
+find /Users/bryce/releaf.static/releaf.site -maxdepth 3 -name "*.html" | xargs -I {} sed -i '' '/<!--/,/-->/d' {}
 
