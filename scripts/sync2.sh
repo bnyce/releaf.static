b5bd8844e705321f218c52a57d9a83ecdb0b6be6
Bryce Benton <brycebenton+pantheon2@gmail.com>
2023-09-05 13:49:31 -0500

    cleanup

--- scripts/sync2.sh
+++ scripts/sync2.sh
@@ -1,4 +1,3 @@
-bash /Users/bryce/releaf.git/scripts/httrack.sh
 bash /Users/bryce/releaf.git/scripts/strip_comments.sh
 bash /Users/bryce/releaf.git/scripts/strip_link.sh
 bash /Users/bryce/releaf.git/scripts/rsync.sh
