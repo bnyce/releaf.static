e8ed56c99ac033be386b7b980544512997278944
Bryce Benton <brycebenton+pantheon2@gmail.com>
2023-09-05 14:39:13 -0500

    cleanup2

--- scripts/httrack.sh
+++ scripts/httrack.sh
@@ -1,2 +1,2 @@
-httrack "https://releaf.site" -O "/Users/bryce/releaf.static" "+*.releaf.site/*.html" "--update" -v 2>> /Users/bryce/releaf-mirror-error-logfile.log | tee -a /Users/bryce/releaf-mirror-logfile.log
+httrack "https://releaf.site" -O "/Users/bryce/releaf.static" "+*.releaf.site/*.html" -N '%n.%t' "--update" -v 2>> /Users/bryce/releaf-mirror-error-logfile.log | tee -a /Users/bryce/releaf-mirror-logfile.log
 
