84ebb172015910422efe9b936bb1d4777fb4780c
Bryce Benton <brycebenton+pantheon2@gmail.com>
2023-09-09 11:47:23 -0500

    updated: Sep09-11:47AM

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,5 +1,12 @@
-bash /Users/bryce/releaf.git/scripts/httrack.sh
+#bash /Users/bryce/releaf.git/scripts/httrack.sh
 bash /Users/bryce/releaf.git/scripts/strip_comments.sh
 bash /Users/bryce/releaf.git/scripts/strip_link.sh
 bash /Users/bryce/releaf.git/scripts/rsync.sh
+
+cd /Users/bryce/releaf.git
+git add .
+git commit -m "updated: $(date '+%b%d-%I:%M%p')"
+
+ssh-add ~/.ssh/pi_rsa
+git push all main
 
