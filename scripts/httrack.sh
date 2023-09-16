274bf59a06d6913ccbe02fd28d13d4598ca22aa0
Bryce Benton <brycebenton+pantheon2@gmail.com>
2023-09-16 11:18:04 -0500

    updated: Sep16-11:18AM

--- scripts/httrack.sh
+++ scripts/httrack.sh
@@ -1,4 +1,2 @@
-
-#httrack "https://releaf.site" -O "/Users/bryce/releaf.static" "+*.releaf.site/*.html" "+*.png" "+*.jpg" "+*.jpeg" "+*.gif" "--update" -v 2>> /Users/bryce/releaf-mirror-error-logfile.log | tee -a /Users/bryce/releaf-mirror-logfile.log
 httrack "https://releaf.site" -O "/Users/bryce/releaf.static" "+*.releaf.site/*.html" "+*.png" "+*.jpg" "+*.jpeg" "+*.gif" -v 2>> /Users/bryce/releaf-mirror-error-logfile.log | tee -a /Users/bryce/releaf-mirror-logfile.log
 
